f560b93d6e4f97e34a1fa2ab2f52e4344dc5891f
xiongfeng <343138759@qq.com>
2022-05-13 20:13:12 +0800

    从字符串中剔除指定字符。

---
+++ type-challenges/Drop Char/readme.md
@@ -0,0 +1,16 @@
+<!--
+ * @Author: xiongfeng '343138759@qq.com'
+ * @Date: 2022-05-13 20:08:12
+ * @LastEditors: xiongfeng '343138759@qq.com'
+ * @LastEditTime: 2022-05-13 20:09:27
+ * @FilePath: \Typescript练习d:\王者农药plus\web前端\ts-challenge\type-challenges\Drop Char\readme.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+
+> 从字符串中剔除指定字符。
+
+例如：
+
+```ts
+type Butterfly = DropChar<" b u t t e r f l y ! ", " ">; // 'butterfly!'
+```
---
+++ type-challenges/Drop Char/template.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: xiongfeng '343138759@qq.com'
+ * @Date: 2022-05-13 20:08:01
+ * @LastEditors: xiongfeng '343138759@qq.com'
+ * @LastEditTime: 2022-05-13 20:13:00
+ * @FilePath: \Typescript练习d:\王者农药plus\web前端\ts-challenge\type-challenges\Drop Char\template.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+type DropChar<
+  S extends string,
+  C extends string
+> = S extends `${infer F}${C}${infer B}` ? DropChar<`${F}${B}`, C> : S;
