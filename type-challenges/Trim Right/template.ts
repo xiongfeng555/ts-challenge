3e22e22e9f8a7104b5294027c3ce5c2e6992263b
xiongfeng <343138759@qq.com>
2022-05-16 19:36:41 +0800

    Implement TrimRight<T> which takes an exact string type and returns a new string with the whitespace ending removed.

---
+++ type-challenges/Trim Right/template.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: xiongfeng '343138759@qq.com'
+ * @Date: 2022-05-16 19:33:00
+ * @LastEditors: xiongfeng '343138759@qq.com'
+ * @LastEditTime: 2022-05-16 19:35:58
+ * @FilePath: \Typescript练习d:\王者农药plus\web前端\ts-challenge\type-challenges\Trim Right\template.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+type TrimRight<S extends string> = S extends `${infer F}${" " | "\n" | "\t"}`
+  ? TrimRight<F>
+  : S;
