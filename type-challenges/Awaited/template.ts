bdf435699deb57b8452016044fedf7750478c621
xiongfeng <343138759@qq.com>
2022-05-24 14:29:37 +0800

    refactor(格式): 格式化代码

--- type-challenges/Awaited/template.ts
+++ type-challenges/Awaited/template.ts
@@ -2,7 +2,7 @@
  * @Author: xiongfeng '343138759@qq.com'
  * @Date: 2022-05-07 19:22:42
  * @LastEditors: xiongfeng '343138759@qq.com'
- * @LastEditTime: 2022-05-07 19:39:10
+ * @LastEditTime: 2022-05-24 11:12:00
  * @FilePath: \Typescript练习d:\王者农药plus\web前端\ts-challenge\type-challenges\Awaited\template.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,4 +10,4 @@ type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer K>
   ? K extends Promise<unknown>
     ? MyAwaited<K>
     : K
-  : never
+  : never;
